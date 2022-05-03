ab63a35d90d2a105d309fbbaa1791f92c634bdd5
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-03 17:55:06 +0300

    Context class has been completed

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -11,7 +11,6 @@ namespace EntityLayer.Concrete
     {
         [Key]
         public int Id { get; set; }
-
         [StringLength(50)]
         public string Name { get; set; }
         [StringLength(50)]
