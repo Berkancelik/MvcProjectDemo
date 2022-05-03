12093f2f2846a220bef87cc7bef03a5d80429bb5
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-03 16:06:00 +0300

    About&Category&Contact entities has been created

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int Id { get; set; }
+        public string Details { get; set; }
+        public string Details2 { get; set; }
+        public string Image { get; set; }
+        public string Image2 { get; set; }
+    }
+}
