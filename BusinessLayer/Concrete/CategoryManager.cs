0c2fb96e384ecf179334ec965cfcd48d2f6e99bf
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-05 11:32:40 +0300

    Some Fixes

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,25 +10,18 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager:ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categoryDal;
 
-        public List<Category> GetAllBL()
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            return repo.List();
+            _categoryDal = categoryDal;
         }
 
-        public void CategpryAddBL(Category p)
+        public List<Category> GetList()
         {
-            if(p.Name == "" || p.Name.Length <=3|| p.Description ==""|| p.Name.Length >= 51)
-            {
-                //error message
-            }
-            else
-            {
-                repo.Insert(p);
-            }
+            return _categoryDal.List();
         }
     }
 }
