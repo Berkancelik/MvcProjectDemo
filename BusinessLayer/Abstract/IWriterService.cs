70f8b8aac63161842029b991428124b197d0f212
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-07 15:33:09 +0300

    author transactions

--- BusinessLayer/Abstract/IWriterService.cs
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    public class IWriterService
+    public interface IWriterService
     {
+        List<Writer> GetList();
+        void Add(Writer writer);
+        Writer GetById(int id);
+        void Delete(Writer writer);
+        void Update(Writer writer);
+
     }
 }
--- BusinessLayer/Concrete/WriterManager.cs
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class WriterManager
+    public class WriterManager : IWriterService
     {
+        IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void Add(Writer writer)
+        {
+            _writerDal.Insert(writer);
+        }
+
+        public void Delete(Writer writer)
+        {
+            _writerDal.Delete(writer);  
+        }
+
+        public Writer GetById(int id)
+        {
+            return _writerDal.Get(x=>x.WriterId == id);
+        }
+
+        public List<Writer> GetList()
+        {
+            return _writerDal.List();
+        }
+
+        public void Update(Writer writer)
+        {
+            _writerDal.Update(writer);  
+        }
     }
 }
