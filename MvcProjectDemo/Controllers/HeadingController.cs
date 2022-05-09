140ed8df26e81a7d0b9c4efc1c7fc6b5ca92946a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-09 10:27:01 +0300

    use of dropdownlist and attachment in header operations

--- MvcProjectDemo/Controllers/HeadingController.cs
+++ MvcProjectDemo/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,32 @@ namespace MvcProjectDemo.Controllers
     public class HeadingController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             var headingValues = hm.GetList();
             return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.Name,
+                                                      Value = x.CategoryId.ToString(),
+                                                  }).ToList();
+            ViewBag.v1 = valueCategory;
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.Add(p);
+            return RedirectToAction("Index");
         }
     }
 }
