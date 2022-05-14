46e07727e74f60a3c76588bc661253b56b441c54
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-14 11:20:43 +0300

    MessageController and Admin Message has been fixed

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void Add(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetList()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "adming@gmail.com");
+        }
+
+        public void Update(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MvcProjectDemo/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager cm = new MessageManager(new EfMessageDal());
+
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var messageList = cm.GetList();
+            return View(messageList);
+        }
+    }
+}
