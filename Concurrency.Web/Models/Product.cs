a6e9f46ce75f2ed3b6c00f8877bbd20899441ce7
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-20 09:46:43 +0300

    Fixed

---
+++ Concurrency.Web/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Concurrency.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
